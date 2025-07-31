05e6d856dcbbc37a627b32c85f6b0fd3de717b10
WillHerondale715 <147683071+WillHerondale715@users.noreply.github.com>
2025-07-31 16:38:33 +0530

    Create assignment.java

---
+++ BasicProgramming/assignment.java
@@ -0,0 +1,25 @@
+public class assignment {
+    public static void main(String[] args) {
+        int x;
+
+        x = 20;
+        x += 5;
+        System.out.println("After x += 5: " + x);
+
+        x = 10;
+        x -= 5;
+        System.out.println("After x -= 5: " + x);
+
+        x = 15;
+        x *= 5;
+        System.out.println("After x *= 5: " + x);
+
+        x = 10;
+        x /= 5;
+        System.out.println("After x /= 5: " + x);
+
+        x = 25;
+        x %= 5;
+        System.out.println("After x %= 5: " + x);
+    }
+}
