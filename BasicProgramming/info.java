897b084f858975e6e8df892f80143a7e26a77fbd
WillHerondale715 <147683071+WillHerondale715@users.noreply.github.com>
2025-07-31 16:57:06 +0530

    Create info.java

---
+++ BasicProgramming/info.java
@@ -0,0 +1,17 @@
+public class info {
+    public static void main(String[] args) {
+        int age = 21;
+        double height = 160.0;
+        char firstInitial = 'R';
+        char lastInitial = 'T';
+        boolean isStudent = true;
+        String fullName = "Rishika Talasila";
+
+        System.out.println("===== Personal Information =====");
+        System.out.println("Name        : " + fullName);
+        System.out.println("Age         : " + age + " years");
+        System.out.println("Height      : " + height + " cm");
+        System.out.println("Initials    : " + firstInitial + " " + lastInitial);
+        System.out.println("Student     : " + isStudent);
+    }
+}
